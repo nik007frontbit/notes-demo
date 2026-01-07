fd7c67406dfad776b4a7de3c0ad5d6d0c8d4b734
neel <neelsutariya12@gmail.com>
2026-01-07 08:59:52 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'core/app_theme.dart';
 import 'features/auth/cubit/auth_cubit.dart';
 import 'features/auth/cubit/auth_state.dart';
